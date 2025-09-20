c25751815bf58008fbaaf74f8f0466db8bae9664
Томбасов Данила Кириллович <124450021+Zenerrium@users.noreply.github.com>
2025-09-20 12:35:49 +0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите номер месяца");
+int a = Convert.ToInt32(Console.ReadLine());
+if (a >= 1 & a <=2 & a == 12) {
+    Console.WriteLine("зима");
+}
+else if (a >= 2 & a <= 5)
+{
+    Console.WriteLine("весна");
+}
+else if (a >= 5 & a <= 8)
+{
+    Console.WriteLine("лето");
+}
+else if (a >= 8 & a <= 11)
+{
+    Console.WriteLine("осень");
+}
+else
+{
+    Console.WriteLine("введен некорректный месяц");
+}
